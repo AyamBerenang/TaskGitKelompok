f2b459992bf430db45106633aedc319cb5e5d041
Jeta Sunanda <sunandajeta@gmail.com>
2025-02-28 12:39:31 +0700

    Update 2 file

--- SwimmingChicken.py
+++ SwimmingChicken.py
@@ -1,4 +1,6 @@
 def swimmingChicken():
     print("Ayam berenang")
+    print("Suatu hari di desa yang indah, terdapat seekor ayam yang sedang berenang di sungai")
+    print("Ayam tersebut sangat menikmati berenang di sungai tersebut")
 
 swimmingChicken()
